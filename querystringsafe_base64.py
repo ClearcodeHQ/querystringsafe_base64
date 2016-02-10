79f7a0eb99b11c2aa44b620e760d37e5bf2aaa09
t.karbownicki <t.karbownicki@clearcode.cc>
2016-02-10 14:19:19 +0100

    Support for python3

--- querystringsafe_base64.py
+++ querystringsafe_base64.py
@@ -19,9 +19,11 @@
 # along with querystringsafe_base64. If not, see <http://www.gnu.org/licenses/
 """Main querystringsafe_base64 module."""
 
+import sys
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 __version__ = '0.1.5'
+PY2 = sys.version_info < (3, 0)
 
 
 def encode(to_encode):
@@ -31,13 +33,16 @@ def encode(to_encode):
     urllib.quote and urllib.quote_plus do not have any effect on the
     result of querystringsafe_base64.encode.
 
-    :param str to_encode:
+    :param (str, bytes) to_encode:
     :rtype: str
     :return: a string that is safe to put as a value in an URL query
         string - like base64, except characters ['+', '/', '='] are
         replaced with ['-', '_', '.'] consequently
     """
-    return urlsafe_b64encode(bytes(to_encode)).replace('=', '.')
+    encoded = urlsafe_b64encode(to_encode).replace(b'=', b'.')
+    if PY2:
+        return encoded
+    return encoded.decode()
 
 
 def decode(encoded):
@@ -51,10 +56,9 @@ def decode(encoded):
         Therefore this function may also be used to decode the regular base64.
 
     :param (str, unicode) encoded: querystringsafe_base64 string or unicode
-    :rtype: str
+    :rtype: str, bytes
     :return: decoded string
     """
-    # If unicode comes in, strange TypeError is thrown from unicode.translate
-    # because it differs from str.translate. Base64 and querystringsafe_base64
-    # are ascii-compatible anyway so convert to str.
-    return urlsafe_b64decode(str(encoded).replace('.', '='))
+    if PY2:
+        return urlsafe_b64decode(str(encoded).replace('.', '='))
+    return urlsafe_b64decode(bytes(encoded, 'ascii').replace(b'.', b'='))
